595abce834013b460197116838a13755f1fecca1
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-10 13:58:48 +0100

    :zap: Select websocket url based on NODE_ENV.

--- plugins/vue-socket.io.js
+++ plugins/vue-socket.io.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueSocketio from 'vue-socket.io'
 
+const url = process.env.NODE_ENV === 'dev'
+  ? '/'
+  : 'https://pictollab.herokuapp.com/'
+
 export default ({ app }) => {
-  if (!process.env.NODE_ENV === 'dev') {
-    Vue.use(VueSocketio, 'https://pictollab.herokuapp.com/', app.store)
-  }
+  if (!process.env.NODE_ENV === 'dev_local')
+    Vue.use(VueSocketio, url, app.store)
 }
