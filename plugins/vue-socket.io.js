390a3f74b4276d1651b06175d31628aecdfe3550
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-11 11:18:07 +0100

    :wrench: Point vue-socket.io to Heroku instance URL.

--- plugins/vue-socket.io.js
+++ plugins/vue-socket.io.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import VueSocketio from 'vue-socket.io'
 
-const url = process.env.NODE_ENV === 'dev'
-  ? '/'
-  : 'https://pictollab.herokuapp.com/'
-
 export default ({ app }) => {
-  if (!process.env.NODE_ENV === 'dev_local')
-    Vue.use(VueSocketio, url, app.store)
+  Vue.use(VueSocketio, 'https://pictollab.herokuapp.com/', app.store)
 }
